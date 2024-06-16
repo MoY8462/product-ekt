0ffea4bbc41856a883ab98af3510cc5529747b5c
Moises Sanchez <moy.sav21@hotmail.com>
2024-06-16 12:22:02 -0600

    Initial Commit

---
+++ PruebaTecnicaElektra/PruebaTecnicaElektraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTecnicaElektraApp.swift
+//  PruebaTecnicaElektra
+//
+//  Created by Moises Sanchez on 16/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTecnicaElektraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
