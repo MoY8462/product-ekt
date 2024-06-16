42c70d8ed0671dfc36f7f2df77dfd16d49d037d3
Moises Sanchez <moy.sav21@hotmail.com>
2024-06-16 13:33:10 -0600

    build: construction project in progress

---
+++ PruebaTecnicaElektra/Models/GetProducto.swift
@@ -0,0 +1,66 @@
+//
+//  Producto.swift
+//  PruebaTecnicaElektra
+//
+//  Created by Moises Sanchez on 16/06/24.
+//
+
+import Foundation
+
+/*
+struct Producto: Identifiable, Codable {
+    let id: String
+    let nombre: String
+    let precio: Double
+    let categoria: String
+    let imagenURL: String
+}*/
+
+// MARK: - Welcome
+struct GetProducto: Codable {
+    let codigo, mensaje, folio, advertencia: String
+    let resultado: Resultado
+}
+
+// MARK: - Resultado
+struct Resultado: Codable {
+    let paginacion: Paginacion
+    let categoria: String
+    let productos: [Producto]
+}
+
+// MARK: - Paginacion
+struct Paginacion: Codable {
+    let pagina, totalPaginas, totalRegistros, totalRegistrosPorPagina: Int
+}
+
+// MARK: - Producto
+struct Producto: Codable {
+    let id: String
+    let idLinea: Int
+    let codigoCategoria: CodigoCategoria
+    let idModalidad, relevancia: Int
+    let lineaCredito: String
+    let pagoSemanalPrincipal, plazoPrincipal: Int
+    let disponibleCredito: Bool
+    let abonosSemanales: [AbonosSemanale]
+    let sku, nombre: String
+    let urlImagenes: [String]
+    let precioRegular: Int
+    let precioFinal, porcentajeDescuento: Double
+    let descuento: Bool
+    let precioCredito, montoDescuento: Double
+}
+
+// MARK: - AbonosSemanale
+struct AbonosSemanale: Codable {
+    let plazo, montoAbono, montoDescuentoAbono, montoUltimoAbono: Int
+    let montoFinalCredito, idPromocion, montoDescuentoElektra, montoDescuentoBanco: Int
+    let precio, montoAbonoDigital: Int
+}
+
+enum CodigoCategoria: String, Codable {
+    case c = "C"
+    case ma = "MA"
+    case tl = "TL"
+}
